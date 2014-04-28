55ea496704e5d609a4d0e029495b6ec1636edefc
Sam Freiberg <sam@netserious.com>
2014-04-27 23:20:18 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+Overview
+========
+basicauth is a simple go library to extract the username and password from basic auth.
+
+Documentation
+=============
+[GoDoc](http://godoc.org/github.com/sfreiberg/basicauth)
---
+++ basicauth.go
@@ -0,0 +1,39 @@
+package basicauth
+
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
+
+// Given a http.Request it will return the username/password from
+// basic auth.
+func GetUserPass(r *http.Request) (username, password string) {
+	authHdr := r.Header.Get("Authorization")
+
+	tokens := strings.Split(authHdr, " ")
+	if len(tokens) != 2 {
+		return
+	}
+
+	if tokens[0] != "Basic" {
+		return
+	}
+
+	decoded, err := base64.StdEncoding.DecodeString(tokens[1])
+	if err != nil {
+		return
+	}
+
+	userPass := strings.SplitN(string(decoded), ":", 2)
+	if len(userPass) == 2 {
+		username = userPass[0]
+		password = userPass[1]
+		return
+	} else if len(userPass) == 1 {
+		username = userPass[0]
+		return
+	}
+
+	return
+}
